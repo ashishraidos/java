8777b0be9856830dea7797ea701c9c3c6a5c8f00
ashishraidos <ashishraidos@gmail.com>
2023-03-17 21:26:51 +0530

    arithmetic operations

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+
+class Main{
+public static void main(String args[]){
+
+int a=5;
+int b=7;
+System.out.println(a+b);
+System.out.println(b-a);
+System.out.println(b/a);
+System.out.println(a*b);
+System.out.println(b%a);}}
